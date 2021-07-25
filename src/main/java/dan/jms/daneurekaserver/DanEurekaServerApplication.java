fa4a7bc3ef8b6bc22ae2779357c3be79f06dbc02
MalenaMoix <moixmalena@gmail.com>
2021-07-25 19:22:50 -0300

    Eureka server configuration

---
+++ src/main/java/dan/jms/daneurekaserver/DanEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package dan.jms.daneurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DanEurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DanEurekaServerApplication.class, args);
+    }
+}
---
+++ src/test/java/dan/jms/daneurekaserver/DanEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dan.jms.daneurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
